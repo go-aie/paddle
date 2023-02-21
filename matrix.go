b47d11dbb5252ab5b2ae43cf8f39135e5b6b14ac
RussellLuo <luopeng.he@gmail.com>
2023-02-21 18:41:20 +0800

    Improve Matrix

--- matrix.go
+++ matrix.go
@@ -2,6 +2,7 @@ package paddle
 
 import (
 	"github.com/go-aie/xslices"
+	"golang.org/x/exp/slices"
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -9,14 +10,19 @@ type Matrix[E xslices.Number] struct {
 	m *mat.Dense
 }
 
-func NewMatrix[E xslices.Number](t Tensor) *Matrix[E] {
+func NewMatrixFromTensor[E xslices.Number](t Tensor) *Matrix[E] {
 	tt := NewTypedTensor[E](t)
 	if len(tt.Shape) != 2 {
 		panic("t is not a matrix")
 	}
 
-	m := mat.NewDense(int(tt.Shape[0]), int(tt.Shape[1]), xslices.NumberToFloat64(tt.Data))
-	return &Matrix[E]{m: m}
+	return NewMatrix(int(tt.Shape[0]), int(tt.Shape[1]), tt.Data)
+}
+
+func NewMatrix[E xslices.Number](r, c int, data []E) *Matrix[E] {
+	return &Matrix[E]{
+		m: mat.NewDense(r, c, xslices.NumberToFloat64(data)),
+	}
 }
 
 func (m *Matrix[E]) Row(i int) []E {
@@ -51,6 +57,40 @@ func (m *Matrix[E]) Cols() [][]E {
 	return cols
 }
 
+func (m *Matrix[E]) Dims() (r, c int) {
+	return m.m.Dims()
+}
+
+func (m *Matrix[E]) Set(rowStart, rowEnd, colStart, colEnd int, value E) *Matrix[E] {
+	return m.SetFunc(rowStart, rowEnd, colStart, colEnd, func(E) E {
+		return value
+	})
+}
+
+func (m *Matrix[E]) SetFunc(rowStart, rowEnd, colStart, colEnd int, f func(E) E) *Matrix[E] {
+	for r := rowStart; r < rowEnd; r++ {
+		for c := colStart; c < colEnd; c++ {
+			v := m.m.At(r, c)
+			newV := f(E(v))
+			m.m.Set(r, c, float64(newV))
+		}
+	}
+	return m
+}
+
+func (m *Matrix[E]) SetAll(value E) *Matrix[E] {
+	return m.SetAllFunc(func(E) E {
+		return value
+	})
+}
+
+func (m *Matrix[E]) SetAllFunc(f func(E) E) *Matrix[E] {
+	r, c := m.m.Dims()
+	return m.SetFunc(0, r, 0, c, func(v E) E {
+		return f(v)
+	})
+}
+
 func (m *Matrix[E]) Norm() *Matrix[E] {
 	r, c := m.m.Dims()
 
@@ -66,6 +106,32 @@ func (m *Matrix[E]) Norm() *Matrix[E] {
 	return m
 }
 
+// Pad creates a new matrix, which expands the receiver m by r rows and c columns,
+// and sets each expanded element to v.
+func (m *Matrix[E]) Pad(r, c int, v E) *Matrix[E] {
+	if r == 0 && c == 0 {
+		return m
+	}
+
+	oldR, oldC := m.m.Dims()
+	r += oldR
+	c += oldC
+
+	newM := NewMatrix[E](r, c, nil).SetAll(v)
+	newM.m.Copy(m.m)
+	return newM
+}
+
 func (m *Matrix[E]) RawData() []E {
 	return xslices.Float64ToNumber[E](m.m.RawMatrix().Data)
+}
+
+// Equal implements the Equal method which will be used by go-cmp to determine equality.
+func (m *Matrix[E]) Equal(other *Matrix[E]) bool {
+	r, c := m.m.Dims()
+	or, oc := other.Dims()
+	if r != or || c != oc {
+		return false
+	}
+	return slices.Equal(m.RawData(), other.RawData())
 }
--- matrix_test.go
+++ matrix_test.go
@@ -21,8 +21,93 @@ func TestMatrix_Norm(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		m := paddle.NewMatrix[float32](tt.inTensor)
+		m := paddle.NewMatrixFromTensor[float32](tt.inTensor)
 		gotData := m.Norm().RawData()
+		if !cmp.Equal(gotData, tt.wantData) {
+			diff := cmp.Diff(gotData, tt.wantData)
+			t.Errorf("Want - Got: %s", diff)
+		}
+	}
+}
+
+func TestMatrix_SetFunc(t *testing.T) {
+	tests := []struct {
+		inTensor paddle.Tensor
+		inOffset []int // rowStart, rowEnd, colStart, colEnd
+		inFunc   func(float32) float32
+		wantData []float32
+	}{
+		{
+			inTensor: paddle.Tensor{
+				Shape: []int32{5, 5},
+				Data: []float32{
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+				},
+			},
+			inOffset: []int{1, 4, 1, 4},
+			inFunc: func(v float32) float32 {
+				return v * v
+			},
+			wantData: []float32{
+				1, 2, 3, 4, 5,
+				1, 4, 9, 16, 5,
+				1, 4, 9, 16, 5,
+				1, 4, 9, 16, 5,
+				1, 2, 3, 4, 5,
+			},
+		},
+	}
+	for _, tt := range tests {
+		m := paddle.NewMatrixFromTensor[float32](tt.inTensor)
+		rowStart, rowEnd, colStart, colEnd := tt.inOffset[0], tt.inOffset[1], tt.inOffset[2], tt.inOffset[3]
+		gotData := m.SetFunc(rowStart, rowEnd, colStart, colEnd, tt.inFunc).RawData()
+		if !cmp.Equal(gotData, tt.wantData) {
+			diff := cmp.Diff(gotData, tt.wantData)
+			t.Errorf("Want - Got: %s", diff)
+		}
+	}
+}
+
+func TestMatrix_Pad(t *testing.T) {
+	tests := []struct {
+		inTensor   paddle.Tensor
+		inPadDims  []int // row, col
+		inPadValue float32
+		wantData   []float32
+	}{
+		{
+			inTensor: paddle.Tensor{
+				Shape: []int32{5, 5},
+				Data: []float32{
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+					1, 2, 3, 4, 5,
+				},
+			},
+			inPadDims:  []int{3, 3},
+			inPadValue: 0,
+			wantData: []float32{
+				1, 2, 3, 4, 5, 0, 0, 0,
+				1, 2, 3, 4, 5, 0, 0, 0,
+				1, 2, 3, 4, 5, 0, 0, 0,
+				1, 2, 3, 4, 5, 0, 0, 0,
+				1, 2, 3, 4, 5, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		m := paddle.NewMatrixFromTensor[float32](tt.inTensor)
+		row, col := tt.inPadDims[0], tt.inPadDims[1]
+		gotData := m.Pad(row, col, tt.inPadValue).RawData()
 		if !cmp.Equal(gotData, tt.wantData) {
 			diff := cmp.Diff(gotData, tt.wantData)
 			t.Errorf("Want - Got: %s", diff)
